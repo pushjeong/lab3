33226188d47ad23e6872d61697a05ea012fa00f2
pushjeong <mineun1206@naver.com>
2024-11-25 21:07:56 +0900

    Delete target directory

--- target/debug/deps/matrix_add-15e6c20b8d849f19.d
+++
@@ -1,5 +0,0 @@
-/root/rust_lab/matrix_add/target/debug/deps/matrix_add-15e6c20b8d849f19: src/main.rs
-
-/root/rust_lab/matrix_add/target/debug/deps/matrix_add-15e6c20b8d849f19.d: src/main.rs
-
-src/main.rs:
--- target/debug/matrix_add.d
+++
@@ -1,1 +0,0 @@
-/root/rust_lab/matrix_add/target/debug/matrix_add: /root/rust_lab/matrix_add/src/main.rs
